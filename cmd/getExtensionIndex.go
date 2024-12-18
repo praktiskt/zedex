72b3db9e68ad8bba1b15e97fee211a35300cb38d
magnus <magnus.furugard@gmail.com>
2024-12-18 14:13:32 +0100

    feat: implement retrieval and display of extension index from Zed client

--- cmd/getExtensionIndex.go
+++ cmd/getExtensionIndex.go
@@ -1,15 +1,29 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"zedex/zed"
+
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
 var getExtensionIndexCmd = &cobra.Command{
 	Use: "extension-index",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("getExtensionIndex called")
+		zc := zed.NewZedClient(1)
+		extensions, err := zc.GetExtensionsIndex()
+		if err != nil {
+			log.Panic(err)
+		}
+
+		extensionsJson, err := json.MarshalIndent(extensions, "", "\t")
+		if err != nil {
+			log.Panic(err)
+		}
+		fmt.Println(string(extensionsJson))
 	},
 }
 
