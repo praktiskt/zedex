d01af830803fb2b98cf776ee193d29531d9552a8
magnus <magnus.furugard@gmail.com>
2024-12-18 13:50:24 +0100

    feat: add functionality to getExtension command to download extension archives and serveProxy command to serve extension API in local mode

--- cmd/getExtension.go
+++ cmd/getExtension.go
@@ -1,18 +1,45 @@
 package cmd
 
 import (
-	"fmt"
+	"os"
+	"path"
 
+	"zedex/utils"
+	"zedex/zed"
+
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
+
+var getExtensionCmdConfig = struct {
+	outputDir string
+}{}
 
 var getExtensionCmd = &cobra.Command{
 	Use: "extension",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("getExtension called")
+		zc := zed.NewZedClient(1)
+		for _, id := range args {
+			log.Infof("(extension=%v) downloading", id)
+			bytes, err := zc.DownloadExtensionArchiveDefault(zed.Extension{ID: id})
+			if err != nil {
+				log.Errorf("(extension=%v) %v", err.Error())
+				continue
+			}
+
+			utils.CreateDirIfNotExists(getExtensionCmdConfig.outputDir)
+
+			err = os.WriteFile(path.Join(getExtensionCmdConfig.outputDir, id+".tar.gz"), bytes, 0o644)
+			if err != nil {
+				log.Errorf("(extension=%v) %v", err.Error())
+				continue
+			}
+			log.Infof("(extension=%v) wrote %v bytes", id, len(bytes))
+		}
 	},
 }
 
 func init() {
 	getCmd.AddCommand(getExtensionCmd)
+	getExtensionCmd.Flags().StringVar(&getExtensionCmdConfig.outputDir, "output-dir", "downloaded-extensions", "output directory")
 }
--- cmd/serveProxy.go
+++ cmd/serveProxy.go
@@ -6,16 +6,20 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// serveProxyCmd represents the serveProxy command
+var serveProxyCmdConfig = struct {
+	localMode bool
+}{}
+
 var serveProxyCmd = &cobra.Command{
 	Use:   "proxy",
-	Short: "Serve a proxy of the Zed API",
+	Short: "Serve a proxy of the Zed extension API.",
 	Run: func(cmd *cobra.Command, args []string) {
 		api := zed.NewAPI()
-		api.Router().Run(":8080")
+		api.Router(serveProxyCmdConfig.localMode).Run(":8080")
 	},
 }
 
 func init() {
 	serveCmd.AddCommand(serveProxyCmd)
+	serveProxyCmd.Flags().BoolVar(&serveProxyCmdConfig.localMode, "local-mode", false, "whether to serve extension list and extensions from a local directly or not")
 }
