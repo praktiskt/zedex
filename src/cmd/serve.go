2a037113111517228f9a09f592339e15172a4e8d
Magnus <magnus.furugard@gmail.com>
2025-05-01 13:01:47 +0200

    feat: make forwarding optional per feature (#3)
    * begin refactoring
    
    * fix(api): update model and input handling
    
    * fix(config): rename hijack flags to enable flags
    
    * fix(serve): add login and edit prediction checks

--- src/cmd/serve.go
+++ src/cmd/serve.go
@@ -10,17 +10,36 @@ import (
 )
 
 var serveCmdConfig = struct {
-	localMode bool
-	outputDir string
-	port      int
+	outputDir            string
+	port                 int
+	enableEditPrediction bool
+	enableLogin          bool
+	enableExtensionStore bool
+	enableReleases       bool
+	enableReleaseNotes   bool
 }{}
 
 var serveCmd = &cobra.Command{
-	Use: "serve",
+	Use:  "serve",
+	Args: cobra.ExactArgs(0),
 	Run: func(cmd *cobra.Command, args []string) {
+		if !serveCmdConfig.enableLogin {
+			log.Fatalf("zedex does not support login forwarding yet")
+		}
+		if !serveCmdConfig.enableEditPrediction {
+			log.Fatalf("zedex does not support edit prediction forwarding yet")
+		}
+
 		zc := zed.NewZedClient(1)
 		zc.WithExtensionsLocalDir(serveCmdConfig.outputDir)
-		api := zed.NewAPI(serveCmdConfig.localMode, zc, serveCmdConfig.port)
+		api := zed.NewAPI(
+			serveCmdConfig.enableExtensionStore,
+			serveCmdConfig.enableLogin,
+			serveCmdConfig.enableEditPrediction,
+			serveCmdConfig.enableReleases,
+			serveCmdConfig.enableReleaseNotes,
+			zc,
+			serveCmdConfig.port)
 
 		log.Infof("serving on %v", serveCmdConfig.port)
 		api.Router().Run(fmt.Sprintf(":%v", serveCmdConfig.port))
@@ -29,7 +48,11 @@ var serveCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(serveCmd)
-	serveCmd.Flags().BoolVar(&serveCmdConfig.localMode, "local-mode", false, "whether to serve extension list and extensions from a local directly or not")
+	serveCmd.Flags().BoolVar(&serveCmdConfig.enableLogin, "enable-login", true, "enable login requests, letting zedex manage them")
+	serveCmd.Flags().BoolVar(&serveCmdConfig.enableEditPrediction, "enable-edit-prediction", true, "enable edit prediction requests, letting zedex manage them")
+	serveCmd.Flags().BoolVar(&serveCmdConfig.enableExtensionStore, "enable-extension-store", true, "enable extension store requests, letting zedex manage them")
+	serveCmd.Flags().BoolVar(&serveCmdConfig.enableReleases, "enable-releases", true, "enable release update requests, letting zedex manage them")
+	serveCmd.Flags().BoolVar(&serveCmdConfig.enableReleaseNotes, "enable-release-notes", true, "enable release note requests, letting zedex manage them")
 	serveCmd.Flags().StringVar(&serveCmdConfig.outputDir, "output-dir", ".zedex-cache", "the directory where local artifacts (index and extensions) are located, ignored if local-mode=false")
 	serveCmd.Flags().IntVar(&serveCmdConfig.port, "port", 8080, "port to serve proxy on")
 }
--- src/zed/api.go
+++ src/zed/api.go
@@ -7,32 +7,56 @@ import (
 )
 
 type API struct {
-	localMode bool
-	zedClient Client
-	port      int
+	enableExtensionStore bool
+	enableLogin          bool
+	enableEditPrediction bool
+	enableReleases       bool
+	enableReleaseNotes   bool
+	zedClient            Client
+	port                 int
 }
 
-func NewAPI(localMode bool, zedClient Client, port int) API {
+func NewAPI(
+	enableExtensionStore bool,
+	enableLogin bool,
+	enableEditPrediction bool,
+	enableReleases bool,
+	enableReleaseNotes bool,
+	zedClient Client,
+	port int,
+) API {
 	return API{
-		zedClient: zedClient,
-		localMode: localMode,
-		port:      port,
+		zedClient:            zedClient,
+		port:                 port,
+		enableExtensionStore: enableExtensionStore,
+		enableLogin:          enableLogin,
+		enableEditPrediction: enableEditPrediction,
+		enableReleases:       enableReleases,
+		enableReleaseNotes:   enableReleaseNotes,
 	}
 }
 
 func (api *API) Router() *gin.Engine {
 	router := gin.Default()
-	controller := NewController(api.localMode, api.zedClient, api.port)
+	controller := NewController(
+		api.enableExtensionStore,
+		api.enableLogin,
+		api.enableEditPrediction,
+		api.enableReleases,
+		api.enableReleaseNotes,
+		api.zedClient,
+		api.port,
+	)
 	router.GET("/extensions", controller.Extensions)
 	router.GET("/extensions/:id/download", controller.DownloadExtension)
 	router.GET("/extensions/:id/:version/download", controller.DownloadExtension)
 
 	router.GET("/api/*path", func(c *gin.Context) {
-		if c.Request.URL.Path == "/api/releases/latest" && api.localMode {
+		if c.Request.URL.Path == "/api/releases/latest" && api.enableReleases {
 			controller.LatestVersion(c)
 			return
 		}
-		if strings.HasPrefix(c.Request.URL.Path, "/api/release_notes/v2/stable/") && api.localMode {
+		if strings.HasPrefix(c.Request.URL.Path, "/api/release_notes/v2/stable/") && api.enableReleaseNotes {
 			controller.LatestReleaseNotes(c)
 			return
 		}
